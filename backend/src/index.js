fa65ce64b8a3b1097662a4ec422df097cf1fb2d4
jcleong <jcleong.edgetech@gmail.com>
2025-05-28 00:36:03 +0800

    Backend code changes commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 
 import authRoutes from "./routes/auth.route.js";
@@ -19,6 +20,12 @@ app.use(express.json());
 
 /****** Need to have this to parse the cookie, so we can get the value of the JWT token ******/
 app.use(cookieParser());
+
+
+app.use(cors({
+    origin: "http://localhost:5173", /****** Allow only this origin, which is the frontend URL *****/
+    credentials: true, /****** Allow the cookies or authorization header to be sent with the request *****/
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { protectRoutes } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
