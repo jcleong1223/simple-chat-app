7311fc717fa52e9df63efeae0576ad990998a451
JC Leong <>
2025-06-06 16:22:13 +0800

    Frontend changes: Changed the app name

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
                             <div className="w-9 h-9 rounded-lg bg-primary/10 flex items-center justify-center">
                                 <MessageSquare className="w-5 h-5 text-primary"/>
                             </div>
-                            <h1 className="text-lg font-bold">ChatBa</h1>
+                            <h1 className="text-lg font-bold">ChatBar</h1>
                         </Link>
                     </div>
 
