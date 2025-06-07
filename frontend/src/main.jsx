f3134d305ee51ca9d5146d3cd4e8cb3c2a7a36e2
JC Leong <>
2025-06-07 10:21:32 +0800

    Frontend changes: Added the comment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,10 @@
 import { StrictMode } from 'react'
+/****** <StrictMode> is a tool for developers.
+ * It helps you find potential problems in your React code during development.
+ * It Detects unsafe lifecycle methods, Warns about usage of deprecated APIs.
+ * Runs certain functions (like useEffect) twice on purpose to help identify side effects that aren’t idempotent.
+******/
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
