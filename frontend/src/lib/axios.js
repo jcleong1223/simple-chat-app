8c5e446b8c6c6589727696642a9efa8cc68bb28d
JC Leong <>
2025-06-07 10:24:51 +0800

    Frontend changes: Added the comment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
+
+    /****** Use import.meta.env.MODE instead of process.env.NODE_ENV because this is the frontend code ******/
     baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
     withCredentials: true,
 });
